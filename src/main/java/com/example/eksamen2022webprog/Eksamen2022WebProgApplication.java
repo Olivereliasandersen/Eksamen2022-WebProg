8d5ce7d6347d7727dd573a8963c050a3436426ac
Olivereliasandersen <oland0229@oslomet.no>
2024-05-03 16:26:31 +0200

    asds

---
+++ src/main/java/com/example/eksamen2022webprog/Eksamen2022WebProgApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamen2022webprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen2022WebProgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen2022WebProgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamen2022webprog/Eksamen2022WebProgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamen2022webprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen2022WebProgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
